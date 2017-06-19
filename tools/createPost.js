9eb86b5b867976642000f2d4217d02382354fe35
Yudai Udagawa <nimius.rd@gmail.com>
2017-06-19 14:53:38 +0900

    Create post easily

---
+++ tools/createPost.js
@@ -0,0 +1,28 @@
+const moment = require('moment');
+const path = require('path');
+const fs = require('fs');
+
+const filename = process.argv[2] || 'sample';
+const post = {
+  date: moment().format('YYYY-MM-DD HH:mm:ss ZZ'),
+  filename: `${moment().format('YYYY-MM-DD')}-${filename}.md`,
+  title: 'New Post!'
+};
+
+const dest = path.join(__dirname, '..', 'src', '_posts', post.filename);
+
+const template =
+`---
+layout: post
+title:  ${post.title}
+date:   ${post.date}
+---
+本文
+`;
+
+fs.writeFile(dest, template, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Create ${dest}`);
+});
