e1c164dafb50f0bd277f75bd7cfff3ce99e937f1
Yudai Udagawa <nimius.rd@gmail.com>
2017-06-24 09:32:42 +0900

    Add error handling

--- tools/updateTime.js
+++ tools/updateTime.js
@@ -9,31 +9,42 @@ const outputFileName = inputFileName.replace(/\d\d\d\d-\d\d-\d\d/, now.format('Y
 const input = path.resolve(__dirname, '..', 'src', '_posts', inputFileName);
 const output = path.resolve(__dirname, '..', 'src', '_posts', outputFileName);
 
-const rl = readline.createInterface({
-  input: fs.createReadStream(input),
-});
-const dest = fs.createWriteStream(output);
-const regexp = /(date:\s*)\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d [+-]\d\d\d\d/;
+fs.stat(inputFileName, (err) => {
+  if (err) {
+    if (err.code === 'ENOENT') {
+      console.error(`${inputFileName}: file does not exist`);
 
-let count = 0;
-rl.on('line', (line) => {
-  if ((/^---$/).test(line)) {
-    count++;
-  }
-
-  if (count < 2 && regexp.exec(line)) {
-    line = line.replace(regexp, `$1${now.format('YYYY-MM-DD HH:mm:ss ZZ')}`);
-  }
-  dest.write(line);
-  dest.write('\n');
-});
-
-rl.on('close', () => {
-  fs.unlink(input, (err) => {
-    if (err) {
-      throw err;
+      return;
     }
-    console.log(`--- ${input}`);
+    throw err;
+  }
+
+  const rl = readline.createInterface({
+    input: fs.createReadStream(input),
   });
-  console.log(`+++ ${output}`);
+  const dest = fs.createWriteStream(output);
+  const regexp = /(date:\s*)\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d [+-]\d\d\d\d/;
+  let count = 0;
+
+  rl.on('line', (line) => {
+    if ((/^---$/).test(line)) {
+      count++;
+    }
+
+    if (count < 2 && regexp.exec(line)) {
+      line = line.replace(regexp, `$1${now.format('YYYY-MM-DD HH:mm:ss ZZ')}`);
+    }
+    dest.write(line);
+    dest.write('\n');
+  });
+
+  rl.on('close', () => {
+    fs.unlink(input, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`--- ${input}`);
+    });
+    console.log(`+++ ${output}`);
+  });
 });
