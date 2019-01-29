fd12e32462a41284afe170fbb1c562cb47f5d268
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-29 10:48:37 +0900

    Update module

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker pull ynuwaiwai/ynuwaiwai.github.io
-docker run -ti --rm -v "$(pwd)":/src -w /src -p 4000:4000 ynuwaiwai/hp bash tools/build.sh
+docker run -ti --rm -v "$(pwd)":/src -w /src -p 4000:4000 ynuwaiwai/ynuwaiwai.github.io bash tools/build.sh
