1c65f60b74e97a54512bf32b53de31ae424da8c1
Yudai Udagawa <nimius.rd@gmail.com>
2019-04-18 01:15:48 +0900

    Fix CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
-const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const cssnano = require('cssnano');
+const path = require('path');
+const postcssCustomMedia = require('postcss-custom-media');
+const postcssInitial = require('postcss-initial');
+const postcssMediaMinmax = require('postcss-media-minmax');
+const postcssNested = require('postcss-nested');
 
 const config = {
   context: path.join(__dirname, 'webpack'),
@@ -12,7 +17,28 @@ const config = {
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          'postcss-loader'
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                postcssNested(),
+                postcssCustomMedia({
+                  importFrom: {
+                    customMedia: {
+                      '--desktop': 'screen and (width > 768px)',
+                      '--mobileL': 'screen and (width <= 425px)',
+                      '--mobileM': 'screen and (width <= 375px)',
+                      '--mobileS': 'screen and (width <= 320px)',
+                      '--tablet': 'screen and (width <= 768px)'
+                    }
+                  }
+                }),
+                postcssMediaMinmax(),
+                postcssInitial(),
+              ]
+            }
+          }
         ]
       }
     ]
@@ -34,6 +60,21 @@ const config = {
 module.exports = (env, argv) => {
   if (argv.mode === 'production') {
     config.plugins.push(new TerserPlugin());
+    config.module.rules[0].use[2].options.plugins.push(
+      cssnano({
+        'autoprefixer': {
+          add: true,
+          browsers: [
+            /* ref: http://browserl.ist/?q=last+2+versions */
+            'last 2 versions',
+            '> 1%'
+          ],
+        },
+        'normalizeCharset': {
+          add: true
+        }
+      })
+    );
   }
 
   return config;
